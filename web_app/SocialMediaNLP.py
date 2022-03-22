7c1b16e56701e347d394072ef029adf924c301c8
ayelrod <lucas25rodriguez@gmail.com>
2022-03-22 16:36:39 -0700

    added basic web app template

---
+++ web_app/SocialMediaNLP.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/alz')
+def alz():
+    return render_template('alz.html')
+
+@app.route('/als-current')
+def als_current():
+    return render_template('als-current.html')
+
+@app.route('/als-past')
+def als_past():
+    return render_template('als-past.html')
+
+@app.route('/nlp')
+def nlp():
+    return render_template('nlp.html')
--- web_scraping/web_scraping/spiders/als_past_caregivers_spider.py
+++ web_scraping/web_scraping/spiders/als_past_caregivers_spider.py
@@ -13,7 +13,7 @@ class AlsPastCaregiverSpider(scrapy.Spider):
     name = "als_past_caregivers"
     start_page = 2 # First page to scrape
     end_page = 23 # Last page to scrape
-    write_to_database = False # If the posts should be written to the database or not
+    write_to_database = True # If the posts should be written to the database or not
     collection_name = "AlsForums-PastCaregivers" # Name of the collection in MongoDB
     
     def start_requests(self):
